de3bbd2251b663390b7dd4db09615093e9ec5fcf
SathwikEnjamuri <93360182+Sathwik9418@users.noreply.github.com>
2024-11-16 12:47:47 +0530

    Update next.config.mjs

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config) => {
-    config.cache = false; // Disable Webpack cache
-    return config;
-  },
-};
 
-module.exports = nextConfig;
+const nextConfig = {};
+
+export default nextConfig;
