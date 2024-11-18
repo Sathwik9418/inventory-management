ed6a10127ce580ad88da4f4dbb195666ca66fe9b
Sathwik9418 <esathwikkumar@gmail.com>
2024-11-19 01:44:28 +0530

    added images to be fetched from s3

--- client/src/app/dashboard/CardPopularProducts.tsx
+++ client/src/app/dashboard/CardPopularProducts.tsx
@@ -25,7 +25,9 @@ const CardPopularProducts = () => {
                 >
                   <div className="flex items-center gap-3">
                       <Image
-                            src={`s3inventorymanagement1.s3.us-east-1.amazonaws.com/product${Math.floor(Math.random()*3)+1}.png`}
+                            src={`https://s3inventorymanagement1.s3.us-east-1.amazonaws.com/product${
+                              Math.floor(Math.random() * 3) + 1
+                            }.png`}
                             alt={product.name}
                             width={48}
                             height={48}
--- client/src/app/products/page.tsx
+++ client/src/app/products/page.tsx
@@ -72,14 +72,16 @@ const Page = () => {
               className="border shadow rounded-md p-4 max-w-full w-full mx-auto"
             >
               <div className="flex flex-col items-center">
-              <Image
-                  src={`s3inventorymanagement1.s3.us-east-1.amazonaws.com/product${Math.floor(Math.random()*3)+1}.png`}
-                  alt={product.name}
-                  width={150}
-                  height={150}
-                  className='mb-3 rounded-2xl w-36 h-36'
+                <Image
+                    src={`https://s3inventorymanagement1.s3.us-east-1.amazonaws.com/product${
+                      Math.floor(Math.random() * 3) + 1
+                    }.png`}
+                    alt={product.name}
+                    width={48}
+                    height={48}
+                    className='rounded-lg w-14 h-14'
 
-              />
+                />
                 <h3 className="text-lg text-gray-900 font-semibold">
                   {product.name}
                 </h3>
